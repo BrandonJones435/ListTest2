b0b032166994df66c0a041f7e77bbd75eabd8fe7
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-03-07 12:36:59 -0700

    Organizing all of my work

---
+++ src/IUSingleLinkedList.java
@@ -0,0 +1,9 @@
+/**
+ * Single-linked-Node-based implementaiton of IndexedUnsortedList
+ * 
+ * @author Brandon Jones CS221-3 Sp2025
+ */
+
+public class IUSingleLinkedList<T> implements IndexedUnsortedList<T> {
+
+}
