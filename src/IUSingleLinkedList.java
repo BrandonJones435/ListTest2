5dc0c0f6dd3a5e76f505e331b3939b4a726f59c1
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-03-07 20:53:54 -0700

    Commit

---
+++ src/IUSingleLinkedList.java
@@ -0,0 +1,132 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * Single-linked-Node-based implementaiton of IndexedUnsortedList
+ * 
+ * @author Brandon Jones CS221-3 Sp2025
+ */
+
+public class IUSingleLinkedList<T> implements IndexedUnsortedList<T> {
+
+    @Override
+    public void add(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void add(int index, T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addAfter(T element, T target) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToFront(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToRear(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean contains(T target) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T first() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T last() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int startingIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T remove(T element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void set(int index, T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
